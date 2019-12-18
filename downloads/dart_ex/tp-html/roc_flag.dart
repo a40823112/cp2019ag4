286e53c425b838c44145f0e9a38b65e61c901811
40823112 <55493510+a40823112@users.noreply.github.com>
2019-12-18 21:39:45 +0800

    Update roc_flag.dart

--- downloads/dart_ex/tp-html/roc_flag.dart
+++ downloads/dart_ex/tp-html/roc_flag.dart
@@ -1,42 +1,34 @@
 import "dart:html";
-// 定義一個 C 轉 F 函式
-C(num a){
- return ((9/5)*a+32); 
-}
-// 定義一個 F 轉 C 函式
-F(num a){
-return (a - 32)*5/9;
-} 
-// 宣告 len 整數變數, 準備設為各字串的長度
-int len;
-// 宣告 var 變數 type, 準備設為各字串最後一個字元, 可能為 C 或 F
-var type;
-// 宣告 var 變數 number, 準備設為各字串中的數字
-var number;
-// 透過HTML,取得使用者輸入的溫度值
-InputElement tempInput = querySelector("#F"); 
-// 將轉換結果顯示在output
-LabelElement output = querySelector("#output");
-void main() {
-   //按下按鈕後推送數值
-  querySelector("#submit").onClick.listen((e) => calcRabbits(tempInput.value));
-}
- calcRabbits(String string){
-  len =  string.length;
-  // 取得各筆資料的最後一個字元
-  type = string[len-1];
-  // 根據溫度類別轉給對應函式進行溫度轉換
-  number = string.substring(0, len-1);
-  // 由於取得的 number 為字串, 必須設法轉為數字 
-  number = int.parse(number);
-  // 之後就可以根據 type 與 number 執行各數列元素的溫度轉換 
-    if(type=="C"){
-      output.innerHtml ="攝氏$number度=華氏${C(number).toStringAsFixed(3)}度";
-    }
-  else if(type=="F"){
-     output.innerHtml ="華氏$number度=攝氏${F(number).toStringAsFixed(3)}度";
-    }
-  else {
-     output.innerHtml ="請輸入數字加上 C 或 F!"
+
+CtoF(num c){
+return c*9/5 + 32;
 }
 
+FtoC(num f){
+return (f - 32)*5/9;
+}
+
+main() {
+    InputElement tempInput = querySelector("#temp");
+    querySelector("#submit").onClick.listen((e) => convert(tempInput.value));
+}
+
+convert(String data){
+  int len;
+  var type;
+  var number;
+  LabelElement output = querySelector("#output");
+  len = data.length;
+  type = data[len-1];
+  number = data.substring(0, len-1);
+  number = int.parse(number);
+  if (type == "C" || type == "c"){
+     output.innerHtml = "攝氏 $number 度 = 華氏
+     ${CtoF(number).toStringAsFixed(2)} 度";
+  } else if ((type == "F" || type == "f")){
+     output.innerHtml = "華氏 $number 度 = 攝氏
+     ${FtoC(number).toStringAsFixed(2)} 度";
+  } else {
+     output.innerHtml = "請輸入數字加上 C 或 F!";
+  }
+} 

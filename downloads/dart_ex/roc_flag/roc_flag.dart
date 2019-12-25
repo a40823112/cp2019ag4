760db9f6beb4a4ec8a3fdecc8612af7e7184a0b2
a40823112 <40823112@gm.nfu.edu.tw>
2019-12-25 21:14:01 +0800

    美國國旗程式修改

--- downloads/dart_ex/roc_flag/roc_flag.dart
+++ downloads/dart_ex/roc_flag/roc_flag.dart
@@ -20,7 +20,7 @@ void main() {
 
 void drawUSA(ctx){
 
-  ctx.clearRect(0, 0, flag_w, flag_h);
+   ctx.clearRect(0, 0, flag_w, flag_h);
   ctx.fillStyle = 'rgb(255, 0, 0)';
   ctx.fillRect(0, 0, flag_w, flag_h);
   ctx.fillStyle = 'rgb(255, 255, 255)';
@@ -38,7 +38,21 @@ void drawUSA(ctx){
   ctx.fillStyle = 'rgb(255, 255, 255)';
   ctx.fillRect(0, 200.4, flag_w / 1, flag_h / 13);
   ctx.fillStyle = 'rgb(0, 0, 150)';
-  ctx.fillRect(0, 0, flag_w / 2, flag_h / 2);
+  ctx.fillRect(0, 0, flag_w / 2, flag_h / 1.9);
+    num b = flag_h/18;
+  ctx.font = "10px Arial";
+  ctx.strokeStyle = 'rgb(255, 255, 255)';
+  ctx.strokeText("✮      ✮      ✮      ✮      ✮      ✮", flag_w / 36,12);
+  ctx.strokeText("✮      ✮      ✮      ✮      ✮", flag_w / 14, b*2);
+  ctx.strokeText("✮      ✮      ✮      ✮      ✮      ✮", flag_w / 36, b*3);
+  ctx.strokeText("✮      ✮      ✮      ✮      ✮", flag_w / 14, b*4);
+  ctx.strokeText("✮      ✮      ✮      ✮      ✮      ✮", flag_w / 36, b*5);
+  ctx.strokeText("✮      ✮      ✮      ✮      ✮", flag_w / 14, b*6);
+  ctx.strokeText("✮      ✮      ✮      ✮      ✮      ✮", flag_w / 36, b*7);
+   ctx.strokeText("✮      ✮      ✮      ✮      ✮", flag_w / 14, b*8);
+   ctx.strokeText("✮      ✮      ✮      ✮      ✮      ✮", flag_w / 36, b*9);
+
+
 
 }
 
